5f6f76fb1d1bff6e1653e9bb2af025ec76e28498
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-05-22 16:04:45 +0200

    Edit comparison mobile friendly

--- web/src/app/[locale]/compare/[id]/compare-edit.tsx
+++ web/src/app/[locale]/compare/[id]/compare-edit.tsx
@@ -3,6 +3,7 @@
 import { EditIcon } from '@/components/icons';
 import { Button } from '@nextui-org/button';
 import { useRouter } from '@/navigation';
+import { Tooltip } from '@nextui-org/react';
 
 interface CompareEditProps {
   id: string;
@@ -15,11 +16,22 @@ export default function CompareEdit({ id }: CompareEditProps) {
   };
   return (
     <div>
+      <Tooltip content='Edit Comparison' color='secondary'>
+        <Button
+          color='primary'
+          startContent={<EditIcon size={14} />}
+          size='sm'
+          onClick={handleEdit}
+          className='md:hidden'
+          isIconOnly
+        />
+      </Tooltip>
       <Button
         color='primary'
         startContent={<EditIcon size={14} />}
         size='sm'
         onClick={handleEdit}
+        className='hidden md:flex'
       >
         Edit Comparison
       </Button>
