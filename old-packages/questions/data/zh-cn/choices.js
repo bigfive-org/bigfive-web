e3ed6c4d7e17d1800de38126a4bc13222d9516c8
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-05-23 22:34:38 +0200

    Fix chinese simplified

--- old-packages/questions/data/zh-cn/choices.js
+++ old-packages/questions/data/zh-cn/choices.js
@@ -6,12 +6,12 @@ module.exports = {
       color: 1
     },
     {
-      text: '比较不准确',
+      text: '不太不准确',
       score: 2,
       color: 2
     },
     {
-      text: '不确定',
+      text: '适中',
       score: 3,
       color: 3
     },
@@ -28,27 +28,27 @@ module.exports = {
   ],
   minus: [
     {
-      text: '非常不准确',
+      text: '非常准确',
       score: 5,
       color: 1
     },
     {
-      text: '比较不准确',
+      text: '比较准确',
       score: 4,
       color: 2
     },
     {
-      text: '不确定',
+      text: '适中',
       score: 3,
       color: 3
     },
     {
-      text: '比较准确',
+      text: '不太不准确',
       score: 2,
       color: 4
     },
     {
-      text: '非常准确',
+      text: '非常不准确',
       score: 1,
       color: 5
     }
