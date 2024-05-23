68337d9e3a8e5f93870f495a50f5a9e76f9f8ec8
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-05-23 15:39:46 +0200

    Add Bengali thanks to Shad Iqbal

---
+++ old-packages/questions/data/bn/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'কখনই না',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'তেমন একটা না',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'অল্প-স্বল্প',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'মোটামুটি',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'সবসময়',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'কখনই না',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'তেমন একটা না',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'অল্প-স্বল্প',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'মোটামুটি',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'সবসময়',
+      score: 1,
+      color: 5
+    }
+  ]
+}
