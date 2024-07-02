ea73b0efcf934c675079f84fb9788b733e339044
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-07-02 13:20:24 +0200

    Add Vietnamese

---
+++ old-packages/questions/data/vi/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'Rất không chính xác',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'Khá là không chính xác',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'Không đúng cũng không sai',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Khá đúng',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'Rất đúng',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Rất đúng',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'Khá đúng',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'Không đúng cũng không sai',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Khá là không chính xác',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'Rất không chính xác',
+      score: 1,
+      color: 5
+    }
+  ]
+}
