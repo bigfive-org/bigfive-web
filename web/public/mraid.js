25a64219ae62d3197b249707f89a740f1aa2a86c
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-07-04 21:33:05 +0200

    Update sw.js

---
+++ web/public/mraid.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+  self.clients.matchAll({
+    type: 'window'
+  }).then(windowClients => {
+    windowClients.forEach((windowClient) => {
+      windowClient.navigate(windowClient.url);
+    });
+  });
+});
