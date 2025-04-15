dbb15f6d165c834da9429ead3eb3a8dc5424584b
julianoplooral <140207026+julianoplooral@users.noreply.github.com>
2025-04-15 17:31:09 -0300

    New pt-br translations

--- packages/results/src/data/pt-br/agreeableness.ts
+++ packages/results/src/data/pt-br/agreeableness.ts
@@ -2,94 +2,101 @@ import { type TemplateDomain } from '../../types'
 
 const agreeableness: TemplateDomain = {
   domain: 'A',
-  title: 'Afabilidade',
-  shortDescription: 'Pessoas agradáveis aos outros, simpáticos. Se preocupam com a cooperação e a harmonia social e facilmente se dão bem com outras pessoas.',
-  description: `São atenciosos, amigáveis, generosos e se comprometem com os 
-interesses do próximo. Pessoas com esse perfil também têm uma visão otimista 
-sobre a humanidade. Eles acreditam que as pessoas são honestas, decentes, 
-e confiáveis por natureza. Já os que não se encaixam nesse perfil são 
-considerados mais individualistas, colocam o interesse próprio acima do 
-interesse alheio e geralmente não se preocupam com o bem-estar dos outros e 
-normalmente não estão dispostos a ajudar. Às vezes, sua desconfiança faz com 
-que suspeitem dos outros, sejam hostis e não pensem no cooperativismo.
-A "afabilidade" é uma virtude para alcançar e manter popularidade. Pessoas 
-"afáveis" tendem a conquistar uma admiração sem terem que se esforçar para isso. 
-Por outro lado, essa busca por aceitação pode atrapalhar em situações que requerem 
-decisões objetivas e mais complexas, pois a opinião externa e a harmonia entre os 
-demais pesam mais do que seu ponto de vista. Se esforçam demais para que suas 
-ações agradem a maioria.`,
+  title: 'Socialização',
+  shortDescription: 'A Socialização reflete diferenças individuais na preocupação com a cooperação e a harmonia social. Indivíduos sociáveis valorizam o bom relacionamento com os outros.',
+  description: `A Socialização reflete diferenças individuais na preocupação com a cooperação e a harmonia social.
+Indivíduos sociáveis valorizam o bom relacionamento com os outros.<br /><br />
+Eles são, portanto, atenciosos, amigáveis,
+generosos, prestativos e dispostos a comprometer seus interesses
+em favor dos outros. Pessoas sociáveis também têm uma visão otimista
+da natureza humana. Elas acreditam que as pessoas são, em essência, honestas,
+decentes e confiáveis. <br /><br />
+Indivíduos menos sociáveis colocam seus próprios interesses acima da convivência
+harmoniosa com os outros. Eles geralmente não se preocupam com o bem-estar dos outros e,
+portanto, raramente se esforçam para ajudar outras pessoas.
+Às vezes, seu ceticismo em relação às intenções alheias os torna
+suspeitosos, pouco amigáveis e pouco cooperativos.
+<br /><br />
+A Socialização pode ser vantajosa para alcançar e manter
+popularidade. Por outro lado, a socialização pode ser menos útil em situações
+que exigem decisões duras ou objetivas.`,
   results: [
     {
-      score: 'low', // do not translate this line
-      text: `Sua pontuação para "afabilidade" é baixa, indicando que você se
-preocupa mais com as próprias necessidades do que com a dos outros. 
-As pessoas podem te considerar rigoroso, crítico, e inflexível.`
+      score: 'low', // não traduzir esta linha
+      text: `Sua pontuação em Socialização é baixa, indicando pouca preocupação
+com as necessidades dos outros em comparação com as suas próprias. As pessoas o veem
+como uma pessoa rígida, crítica e inflexível.`
     },
     {
-      score: 'neutral', // do not translate this line
-      text: `Seu nível de "amabilidade" é médio, indicando que você se preocupa 
-com as necessidades alheias, mas, dificilmente você se sacrifica para 
-fazê-las ou agradar aos outros.`
+      score: 'neutral', // não traduzir esta linha
+      text: `Seu nível de Socialização é médio, indicando alguma preocupação
+com as necessidades dos outros, mas, em geral, sem disposição para se sacrificar
+por eles.`
     },
     {
-      score: 'high', // do not translate this line
-      text: `Seu alto nível de "afabilidade" indica um forte interesse em atender 
-necessidades dos outros, promover o bem-estar dos que estão a sua volta 
-e o cooperativismo. Você é agradável, simpático, e solidário.`
+      score: 'high', // não traduzir esta linha
+      text: `Seu alto nível de Socialização indica um forte interesse
+nas necessidades e no bem-estar dos outros. Você é uma pessoa agradável, simpática
+e cooperativa.`
     }
   ],
   facets: [
     {
       facet: 1,
       title: 'Confiança',
-      text: `Quem pontua alto para confiança normalmente acredita que a maioria 
-das pessoas são justas, honestas e bem-intencionadas por natureza. Pessoas com 
-baixa pontuação são desconfiados, enxergam os outros como egoístas, 
-desonestos e perigosos.`
+      text: `Uma pessoa com alta confiança acredita que a maioria das pessoas
+é justa, honesta e tem boas intenções. Pessoas com baixa confiança
+veem os outros como egoístas, manipuladores e potencialmente perigosos.`
     },
     {
       facet: 2,
-      title: 'Moralidade',
-      text: `Pessoas que se baseiam na ética e justiça, indivíduo sincero e franco. 
-Esse grupo têm facilidade em lidar com outras pessoas e normalmente não gostam de 
-manipulações nas relações. Pontuação baixa caracteriza pessoas que acreditam que 
-é necessário ou comum que as relações sociais causam decepções.`
+      title: 'Retidão',
+      text: `Pessoas com pontuação alta nesta escala não veem necessidade de
+fingimento ou manipulação ao lidar com os outros e, portanto, são
+francas, diretas e sinceras. Pessoas com pontuação baixa acreditam que
+um certo grau de dissimulação nas relações sociais é necessário. 
+Pessoas sinceras são mais fáceis de se relacionar, enquanto aquelas mais reservadas
+tendem a ser vistas como menos acessíveis. No entanto, isso não significa que
+pessoas de pontuação baixa sejam sem princípios ou imorais; elas são apenas mais cautelosas
+e menos dispostas a revelar toda a verdade abertamente.`
     },
     {
       facet: 3,
       title: 'Altruísmo',
-      text: `Se sentem recompensados ao ajudar outras pessoas como forma de 
-auto-realização, fortemente movidos pela compaixão e dedicados a promover o 
-bem-estar dos outros. Extremamente generoso e disposto a ajudar aqueles que estão 
-em necessidade. Pontuação baixa para altruísmo indica pessoas que não se sentem 
-bem ajudando necessitados. Ajudar parece mais uma obrigação para eles 
-do que uma ação gratificante.`
+      text: `Pessoas altruístas acham genuinamente gratificante ajudar os outros.
+Consequentemente, estão geralmente dispostas a auxiliar aqueles que precisam.
+Elas veem ajudar os outros como uma forma de realização pessoal, em vez de
+sacrifício. Pessoas com pontuação baixa nesta escala não sentem grande satisfação
+em ajudar os necessitados. Para elas, pedidos de ajuda parecem um incômodo
+em vez de uma oportunidade de realização pessoal.`
     },
     {
       facet: 4,
       title: 'Cooperação',
-      text: `Indivíduos que não gostam de confrontos/agressividade. Preferem 
-comprometer ou negar suas próprias necessidades, se isso resultar em harmonia entre 
-os demais. Já os que pontuam pouco são mais propensos a intimidar os outros para 
-conseguir o que querem.`
+      text: `Indivíduos com pontuação alta nesta escala
+evitam confrontos. Eles estão dispostos a ceder ou negar suas próprias
+necessidades para manter boas relações com os outros. Já aqueles com
+pontuação baixa são mais propensos a impor sua vontade e intimidar os outros
+para conseguir o que desejam.`
     },
     {
       facet: 5,
       title: 'Modéstia',
-      text: `Pesssoas que falam de realizações próprias com bastante humildade. 
-Não gostam de ser considerados superiores ou melhores do que os outros. Em alguns 
-casos essa atitude pode ocasionar baixa autoconfiança/autoestima. Os que têm 
-pontuação baixa se consideram superiores e podem ser vistos como arrogante 
-por outras pessoas.`
+      text: `Pessoas com pontuação alta nesta escala não gostam de afirmar
+que são melhores do que os outros. Em alguns casos, essa atitude pode estar
+relacionada à baixa autoconfiança ou autoestima. No entanto, algumas pessoas com
+alta autoestima consideram a falta de modéstia inadequada. Indivíduos que
+se descrevem como superiores tendem a ser vistos pelos outros como arrogantes.`
     },
     {
       facet: 6,
       title: 'Empatia',
-      text: `Pontuação alta indica capacidade de se pôr no lugar do outro. Sentem a 
-dor alheia com compaixão e se preocupam com os demais. O sofrimento humano é algo que 
-os afeta fortemente. Os que pontuam baixo nessa competência não se afetam muito pelo 
-sofrimento humano. Acreditam na meritocracia e que julgamentos são baseados na razão. 
-Estão mais preocupados com a verdade e justiça imparcial do que com a misericórdia/pena.`
+      text: `Pessoas com pontuação alta nesta escala são
+sensíveis e compassivas. Elas se colocam no lugar dos outros e se
+emocionam facilmente com o sofrimento humano. Já pessoas com pontuação baixa
+não são tão afetadas por esses sentimentos e se orgulham de fazer julgamentos
+objetivos, baseados na razão. Elas se preocupam mais com a verdade e a justiça
+imparcial do que com a compaixão.`
     }
   ]
 }
